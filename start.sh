f3f66810d1e6a62bc189ade3790a5af3e9e54bfc
Marcin Lason <marcin.lason1@gmail.com>
2020-06-27 21:04:40 +0100

    Add HBase do the project

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#Starting HBase
+~/Documents/Kariera/Technologies/HBase/hbase-2.2.4/bin/start-hbase.sh
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#Stopping HBase
+~/Documents/Kariera/Technologies/HBase/hbase-2.2.4/bin/stop-hbase.sh
